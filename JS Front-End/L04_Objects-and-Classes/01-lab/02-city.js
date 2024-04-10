b92c9c1b3b7a9532482d71ba6fcd934d3d948a56
Trayana Boykova <trayana.boykova@gmail.com>
2024-04-10 16:04:43 +0300

    Update 02-city.js

--- JS Front-End/L04_Objects-and-Classes/01-lab/02-city.js
+++ JS Front-End/L04_Objects-and-Classes/01-lab/02-city.js
@@ -1,6 +1,5 @@
-function printObjectProperties(obj) {
-    for (const [key, value] of Object.entries(obj)) {
-      console.log(`${key} -> ${value}`);
+function printCityInfo(city) {
+    for (let key in city) {
+        console.log(`${key} -> ${city[key]}`);
     }
-  }
-  
+}
